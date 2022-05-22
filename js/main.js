881b6f797cecc8ad28ef445d2fb10b0b7f7af513
Alex-01984 <Rubik10965@yandex.ru>
2022-05-22 13:37:36 +0300

    add: create map

--- js/main.js
+++ js/main.js
@@ -10,12 +10,13 @@ const swiper = new Swiper('.swiper', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
   },
 
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+  effect: 'slide',
 });
